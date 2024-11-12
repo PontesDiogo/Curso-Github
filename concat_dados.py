fc78dafdf92ee50ae1a451e30f8691cdd2e37ecb
deogo <diogogpontes@gmail.com>
2024-11-12 11:06:11 -0300

    Commit 2 - Desafio 2 Python

--- concat_dados.py
+++
@@ -1,8 +0,0 @@
-#receber dois dados do usuário e os concatenar
-info1 = input("Digite a primeira informação: ")
-info2 = input("Digite a primeira informação: ")
-
-
-info_concatenada = info1 + " " + info2
-
-print("As informações concatenadas são: ", info_concatenada)
